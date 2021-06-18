1ca8e716132e45fa01d51d68f6a5b83b88337ca3
diegun_99 <diegun123456789@gmail.com>
2021-06-17 20:21:45 -0500

    primerosPasos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Empleado } from './models/empleado.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'appEmpleados';
+  title = 'Listado de empleados';
+
+  empleados:Empleado[] =[
+    new Empleado("Diego","Corzo","presidente",3000000),
+    new Empleado("Juan","lala","ejecutivo",400000),
+    new Empleado("Carlos","Cor","licenciado",650000),
+    new Empleado("Cesar","martin","aprendiz",2000),
+  ];
 }
